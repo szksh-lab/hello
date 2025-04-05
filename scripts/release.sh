ede16353031f899fa47151264daa787fd239a29d
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-05 16:10:53 +0900

    ci: release homebrew from local (#9)
    * ci: release homebrew from local
    
    * [autofix.ci] apply automated fixes
    
    * ci: remove an input
    
    * ci: fix path
    
    * support scoop
    
    * ci: fix path
    
    * ci: fix
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ scripts/release.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+version=$1
+
+WORKFLOW=${WORKFLOW:-release.yaml}
+GITHUB_SERVER_URL=${GITHUB_SERVER_URL:-https://github.com}
+GITHUB_REPOSITORY=$(gh repo view --json nameWithOwner --jq ".nameWithOwner")
+GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY%/*}
+GITHUB_REPOSITORY_NAME=${GITHUB_REPOSITORY#*/}
+
+git tag -m "chore: release $version" "$version"
+git push origin "$version"
+
+if [[ "$version" =~ - ]]; then
+    exit 0
+fi
+
+# Get a workflow run id
+sleep 10 # Wait for the workflow run to start
+run_id=$(gh run list -w "$WORKFLOW" -L 1 --json databaseId --jq '.[].databaseId')
+
+# Wait until the workflow run completes
+gh run watch --exit-status "$run_id"
+
+# Work on a temporary directory
+tempdir=$(mktemp -d)
+echo "[INFO] Temporary directory: $tempdir" >&2
+cd "$tempdir"
+# Download the GitHub Actions Artifact
+echo "[INFO] Downloading GitHub Actions Artifact" >&2
+gh run download -R "$GITHUB_REPOSITORY" "$run_id" --pattern goreleaser
+
+# Push homebrew
+echo "[INFO] Checking out homebrew-${GITHUB_REPOSITORY_NAME}" >&2
+git clone --depth 1 "$GITHUB_SERVER_URL/${GITHUB_REPOSITORY_OWNER}/homebrew-${GITHUB_REPOSITORY_NAME}"
+cp goreleaser/homebrew/*.rb "homebrew-${GITHUB_REPOSITORY_NAME}"
+pushd "homebrew-${GITHUB_REPOSITORY_NAME}"
+echo "[INFO] Commit and push homebrew-${GITHUB_REPOSITORY_NAME}" >&2
+git add *.rb
+git commit -m "Brew formula update for $GITHUB_REPOSITORY_NAME version $version"
+git push origin main
+popd
+
+# Push scoop
+echo "[INFO] Checking out scoop-bucket" >&2
+git clone --depth 1 "$GITHUB_SERVER_URL/${GITHUB_REPOSITORY_OWNER}/scoop-bucket"
+cp goreleaser/scoop/*.json "scoop-bucket"
+pushd "scoop-bucket"
+echo "[INFO] Commit and push scoop" >&2
+git add *.json
+git commit -m "Scoop update for $GITHUB_REPOSITORY_NAME version $version"
+git push origin main
